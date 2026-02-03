644eae439587378dc45aa5802290d15b4f47b425
Bruno Tenorio <tenoriobn@gmail.com>
2026-02-03 10:25:18 -0300

    feat: :sparkles: Ajusta ciclo de vida de um service worker

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = "meu_cache";
+
+self.addEventListener("install", (event) => {
+  console.log("Instalando o service worker");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(["/index.html"]).then(() => self.skipWaiting());
+    }),
+  );
+});
