b78fd8e0f276b9ec8643d2fe3af353661af5977a
Bruno Tenorio <tenoriobn@gmail.com>
2026-02-09 09:14:22 -0300

    feat: :sparkles: feat: :sparkles: Aplica estratégia Cache-First no service worker

--- public/sw.js
+++ public/sw.js
@@ -11,4 +11,23 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
   console.log(`Baixando ${event.request.url}`);
+  event.respondWith(cachePrimeiro(event.request));
 });
+
+const cachePrimeiro = async (request) => {
+  const respostaDoCache = await caches.match(request);
+
+  if (respostaDoCache) {
+    return respostaDoCache;
+  }
+
+  const respostaRede = await fetch(request);
+  atualizaCache(request, respostaRede.clone());
+
+  return respostaRede;
+};
+
+const atualizaCache = async (request, response) => {
+  const cache = await caches.open(CACHE_NAME);
+  await cache.put(request, response);
+};
